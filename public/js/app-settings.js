222df9859ddc41666bd580ce1354bec61790944e
BrightObengFianko <108421570+BrightObengFianko@users.noreply.github.com>
2026-09-20 16:48:36 +0000

    fix: use RecorDs as installed app name

--- public/js/app-settings.js
+++ public/js/app-settings.js
@@ -69,7 +69,7 @@
         }
 
         if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/service-worker.js?v=10", {
+        navigator.serviceWorker.register("/service-worker.js?v=11", {
                 updateViaCache: "none"
             }).then(registration => {
                 registration.update();
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "records-shell-v10";
+const CACHE_NAME = "records-shell-v11";
 const APP_SHELL = [
     "/",
     "/index.html",
