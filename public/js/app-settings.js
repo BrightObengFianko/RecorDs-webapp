c8e8fce2502c14f0b83a6dd9909479ee28cd23a7
BrightObengFianko <108421570+BrightObengFianko@users.noreply.github.com>
2026-09-21 11:54:55 +0000

    fix: fit mobile registration date input

--- public/js/app-settings.js
+++ public/js/app-settings.js
@@ -69,7 +69,7 @@
         }
 
         if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/service-worker.js?v=24", {
+        navigator.serviceWorker.register("/service-worker.js?v=25", {
                 updateViaCache: "none"
             }).then(registration => {
                 // Do not make offline startup wait on a service-worker update check.
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "records-shell-v24";
+const CACHE_NAME = "records-shell-v25";
 const APP_SHELL = [
     "/",
     "/index.html",
